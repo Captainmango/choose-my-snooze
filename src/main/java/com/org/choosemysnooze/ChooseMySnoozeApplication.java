48ab14a95b457dae98268495dc8c5ba63631b224
Edward Heaver <edward.heaver01@gmail.com>
2022-11-06 18:31:52 +0000

    init commit

---
+++ src/main/java/com/org/choosemysnooze/ChooseMySnoozeApplication.java
@@ -0,0 +1,13 @@
+package com.org.choosemysnooze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseMySnoozeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseMySnoozeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/choosemysnooze/ChooseMySnoozeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.org.choosemysnooze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseMySnoozeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
