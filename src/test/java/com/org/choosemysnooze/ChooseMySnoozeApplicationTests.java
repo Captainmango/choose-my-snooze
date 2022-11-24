b65f68b213b43c4d5e1a50cbd5cfc4c55c5888c3
Edward Heaver <edward.heaver01@gmail.com>
2022-11-24 16:01:54 +0000

    worked out issue with test. context problem. implementing test containers

--- src/test/java/com/org/choosemysnooze/ChooseMySnoozeApplicationTests.java
+++ src/test/java/com/org/choosemysnooze/ChooseMySnoozeApplicationTests.java
@@ -1,12 +1,20 @@
 package com.org.choosemysnooze;
 
+import com.org.choosemysnooze.api.controllers.orders.OrdersController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ChooseMySnoozeApplicationTests {
 
+	@Autowired
+	private OrdersController ordersController;
+
 	@Test
 	void contextLoads() {
+		assertThat(ordersController).isNotNull();
 	}
 }
