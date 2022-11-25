9919aff87809a648d2f13cf2ff55d95d20617b16
Edward Heaver <edward.heaver01@gmail.com>
2022-11-25 12:40:50 +0000

    Still working on this. Using different image still has issues. May need outside help

--- src/test/java/com/org/choosemysnooze/IntegratedKeycloakTest.java
+++ src/test/java/com/org/choosemysnooze/IntegratedKeycloakTest.java
@@ -1,15 +1,18 @@
 package com.org.choosemysnooze;
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.ClassRule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.io.File;
 import java.util.Map;
 
 @Testcontainers
@@ -17,34 +20,36 @@ public abstract class IntegratedKeycloakTest
 {
     @Value("${server.keycloak-db-host}")
     private static String keycloakDbHost;
-    private static final Network myNetwork = Network.newNetwork();
+
+//    @ClassRule
+//    public static final Network myNetwork = Network.newNetwork();
+//
+//    @Container
+//    private static final GenericContainer<?> postgreSQLContainer = new GenericContainer<>(DockerImageName.parse("bitnami/postgresql:latest"))
+//            .withNetwork(myNetwork)
+//            .withNetworkAliases("postgresql")
+//            .withEnv("POSTGRESQL_USERNAME", "admin")
+//            .withEnv("POSTGRESQL_PASSWORD", "admin")
+//            .withEnv("POSTGRESQL_DATABASE", "keycloak-db")
+//            .withExposedPorts(5432);
 
     @Container
-    private static final GenericContainer<?> postgreSQLContainer = new GenericContainer<>(DockerImageName.parse("bitnami/postgresql:latest"))
-            .withNetwork(myNetwork)
-            .withEnv(Map.of(
-                    "POSTGRESQL_USERNAME", "admin",
-                    "POSTGRESQL_PASSWORD", "admin",
-                    "POSTGRESQL_DATABASE", "keycloak-db"
-            ));
-
-    @Container
-    private static final KeycloakContainer keycloak = new KeycloakContainer("bitnami/keycloak:latest")
-            .withNetwork(myNetwork)
+    private static final KeycloakContainer keycloak = new KeycloakContainer()
+//            .withNetwork(myNetwork)
             .withRealmImportFile("/realm-export.json")
-            .withEnv("KEYCLOAK_DATABASE_NAME", "keycloak-db")
-            .withEnv("KEYCLOAK_DATABASE_HOST", keycloakDbHost)
-            .withEnv("KEYCLOAK_DATABASE_USER", "admin")
-            .withEnv("KEYCLOAK_DATABASE_PASSWORD", "admin")
+//            .withEnv("KEYCLOAK_DATABASE_NAME", "keycloak-db")
+//            .withEnv("KEYCLOAK_DATABASE_HOST", "postgresql")
+//            .withEnv("KEYCLOAK_DATABASE_PORT", "5432")
+//            .withEnv("KEYCLOAK_DATABASE_USER", "admin")
+//            .withEnv("KEYCLOAK_DATABASE_PASSWORD", "admin")
             .withEnv("KEYCLOAK_ADMIN_USER", "admin")
-            .withEnv("KEYCLOAK_ADMIN_PASSWORD", "password");
+            .withEnv("KEYCLOAK_ADMIN_PASSWORD", "password")
+            .withExposedPorts(8080);
+
 
     @DynamicPropertySource
     static void registerNewIssuerUri(DynamicPropertyRegistry registry)
     {
-        registry.add("spring.security.client.provider.keycloak.issuer-uri",
-                () -> keycloak.getAuthServerUrl() + "/realms/choose-my-snooze");
-
-        registry.add("server.keycloak-db-host", postgreSQLContainer::getContainerName);
+        registry.add("spring.security.client.provider.keycloak.issuer-uri", () -> keycloak.getAuthServerUrl() + "/realms/master");
     }
 }
