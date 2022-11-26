60fcf7f918ead1183386a66b4a2206cf68d4ab09
Edward Heaver <edward.heaver01@gmail.com>
2022-11-26 17:56:21 +0000

    Clean up

--- src/test/java/com/org/choosemysnooze/ChooseMySnoozeApplicationTests.java
+++ src/test/java/com/org/choosemysnooze/ChooseMySnoozeApplicationTests.java
@@ -4,8 +4,6 @@ import com.org.choosemysnooze.api.controllers.orders.OrdersController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -19,9 +17,6 @@ class ChooseMySnoozeApplicationTests extends IntegratedKeycloakTest {
 	@Test
 	@WithMockUser
 	void contextLoads() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println(auth.getPrincipal());
-
 		assertNotNull(ordersController);
 	}
 }
